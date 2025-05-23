ef591049dbeed4736ee70b0291339f320370f0b8
AngelYQ1 <angel.yucra.q@tecsup.edu.pe>
2025-05-23 10:01:26 -0500

    Cambio realizado en ProductResponse

--- app/src/main/java/com/example/ejercicio09/Models/ProductResponse.kt
+++ app/src/main/java/com/example/ejercicio09/Models/ProductResponse.kt
@@ -1,1 +1,8 @@
+package com.example.ejercicio09.Models
 
+import com.google.gson.annotations.SerializedName
+
+data class ProductResponse(
+    @SerializedName("products")
+    val products: List<ProductModel>
+)
